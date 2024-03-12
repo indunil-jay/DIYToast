ee67814955267b4fbfe3d7f758cf1bd56e516163
induniljay <indunil.jay@gmail.com>
2024-03-12 18:45:06 +0530

    dynamically change data updated

--- dist/toast.js
+++ dist/toast.js
@@ -8,22 +8,25 @@ var SVG;
 })(SVG || (SVG = {}));
 class Toast {
     constructor(options) {
-        this.toastContainer = document.querySelector(`.toast__container`);
-        this.options = options;
+        this.defaultOptions = {
+            position: "top-right",
+            text: "",
+            type: "success",
+            autoClose: false
+        };
+        this.options = Object.assign(Object.assign({}, this.defaultOptions), options);
+        this.toastContainer = createContainer(this.options.position);
         Object.entries(this.options).forEach(([key, value]) => {
             this[key] = value;
         });
         this.close();
     }
     set position(value) {
-        // If the toast container already exists, update its position
         if (this.toastContainer) {
-            // Set the data-position attribute
             this.toastContainer.setAttribute("data-position", value);
             this.toastContainer.insertAdjacentHTML("beforeend", this.markup());
         }
         else {
-            // Otherwise, create the container with the specified position
             this.toastContainer = createContainer(value);
             this.toastContainer.insertAdjacentHTML("beforeend", this.markup());
         }
@@ -37,6 +40,11 @@ class Toast {
     set text(value) {
         document.querySelector(`.toast__messageBox .message .body-text`).textContent = value;
     }
+    set autoClose(value) {
+        if (value) {
+            setTimeout(() => { this.remove(); }, 5000);
+        }
+    }
     remove() {
         if (this.toastContainer) {
             this.toastContainer.remove();
@@ -44,7 +52,7 @@ class Toast {
     }
     close() {
         document.querySelector('.icon__close').addEventListener('click', e => {
-            this.toastContainer.style.display = 'none';
+            this.toastContainer.remove();
         });
     }
     markup() {
@@ -58,7 +66,7 @@ class Toast {
         if (this.options.type === 'warning')
             iconName = SVG.warning;
         return `<div class="toast__messageBox">
-        <!-- iocn -->
+        <!-- icon -->
         <div class="icon__wrapper">
           <div class="icon__overlay "></div>
           <svg class="icon__picture">
@@ -68,17 +76,16 @@ class Toast {
 
         <!-- message -->
         <div class="message">
-          <h3 class="title-text">Success</h3>
-          <p class="body-text"> &nbsp;</p>
+          <h3 class="title-text">${this.options.type || "Success"}</h3>
+          <p class="body-text">${this.options.text || "&nbsp;"}</p>
         </div>
-        <!-- close btn -->
+        <!-- close button -->
         <div class="icon__wrapper">
           <svg class="icon__close">
             <use xlink:href="image/sprite.svg#icon-cancel-circle"></use>
           </svg>
         </div>
-      </div>
-    </div> `;
+      </div>`;
     }
 }
 function createContainer(position) {
@@ -88,7 +95,4 @@ function createContainer(position) {
     document.body.append(container);
     return container;
 }
-const toast = new Toast({ position: "top-right", text: "Hello world!", type: 'info' });
-// setTimeout(()=>{
-//   toast.remove()
-// },1000)
+const toast = new Toast({ position: "top-right", text: "Hello world!", type: 'success', autoClose: true });
--- src/toast.ts
+++ src/toast.ts
@@ -1,86 +1,93 @@
 interface Options {
-  position:
+  position?:
     | "top-right"
     | "top-left"
     | "bottom-right"
     | "bottom-left"
     | "top-center"
     | "bottom-center";
-  text: string;
-  type:string;
+  text?: string;
+  type?: string;
+  autoClose?: boolean;
 }
-enum SVG{
-  success ="check-alt",
-  error ="x-altx-alt",
-  info="notification",
-  warning="warning"
+
+enum SVG {
+  success = "check-alt",
+  error = "x-altx-alt",
+  info = "notification",
+  warning = "warning"
 }
 
 class Toast {
   private options: Options;
-  private toastContainer : HTMLDivElement=document.querySelector(`.toast__container`)!
+  private toastContainer: HTMLDivElement;
 
-  constructor(options: Options) {
-    this.options = options;
-    Object.entries(this.options).forEach(([key, value]) => {
-      this[key as keyof Toast] = value;
-    });
+  private defaultOptions: Options = {
+    position: "top-right",
+    text: "",
+    type: "success",
+    autoClose: false
+  };
 
-    this.close()
-  }
+constructor(options: Options) {
+  this.options = { ...this.defaultOptions, ...options };
+  this.toastContainer = createContainer(this.options.position!);
 
-set position(value: string) {
-  // If the toast container already exists, update its position
-  if (this.toastContainer) {
-  // Set the data-position attribute
-   this.toastContainer.setAttribute("data-position", value);
-    this.toastContainer.insertAdjacentHTML("beforeend", this.markup());
-  } else {
-    // Otherwise, create the container with the specified position
-    this.toastContainer = createContainer(value);
-    this.toastContainer.insertAdjacentHTML("beforeend", this.markup());
-  }
+  Object.entries(this.options).forEach(([key, value]) => {
+    (this[key as keyof Toast] as any) = value;
+  });
+
+  this.close();
 }
 
-  
-set type( value:string){
- 
-    this.toastContainer.classList.add(`toast__container-${value}`)
-    document.querySelector('.icon__overlay')!.classList.add(`icon__overlay-${value}`)
-    document.querySelector('.icon__picture')!.classList.add(`icon__picture-${value}`)
-    document.querySelector('.toast__messageBox .message .title-text')!.textContent = value
+  set position(value: string) {
+    if (this.toastContainer) {
+      this.toastContainer.setAttribute("data-position", value);
+      this.toastContainer.insertAdjacentHTML("beforeend", this.markup());
+    } else {
+      this.toastContainer = createContainer(value);
+      this.toastContainer.insertAdjacentHTML("beforeend", this.markup());
+    }
   }
 
-set text(value: string) {
+  set type(value: string) {
+    this.toastContainer.classList.add(`toast__container-${value}`);
+    document.querySelector('.icon__overlay')!.classList.add(`icon__overlay-${value}`);
+    document.querySelector('.icon__picture')!.classList.add(`icon__picture-${value}`);
+    document.querySelector('.toast__messageBox .message .title-text')!.textContent = value;
+  }
+
+  set text(value: string) {
     document.querySelector(`.toast__messageBox .message .body-text`)!.textContent = value;
-}
+  }
 
+  set autoClose(value: boolean) {
+    if (value) {
+      setTimeout(() => { this.remove(); }, 5000);
+    }
+  }
 
-
-public remove() {
-    
+  public remove() {
     if (this.toastContainer) {
       this.toastContainer.remove();
     }
   }
- 
 
-private close(){
-  document.querySelector('.icon__close')!.addEventListener('click', e=>{
-    this.toastContainer.style.display = 'none'
-  })
- }
+  private close() {
+    document.querySelector('.icon__close')!.addEventListener('click', e => {
+      this.toastContainer.remove();
+    });
+  }
 
-
-markup() {
-  let iconName : SVG=SVG.success;
-   if(this.options.type === 'success') iconName =SVG.success
-   if(this.options.type === 'error') iconName =SVG.error
-   if(this.options.type === 'info') iconName =SVG.info
-   if(this.options.type === 'warning') iconName =SVG.warning
+  private markup() {
+    let iconName: SVG = SVG.success;
+    if (this.options.type === 'success') iconName = SVG.success;
+    if (this.options.type === 'error') iconName = SVG.error;
+    if (this.options.type === 'info') iconName = SVG.info;
+    if (this.options.type === 'warning') iconName = SVG.warning;
 
     return `<div class="toast__messageBox">
-        <!-- iocn -->
+        <!-- icon -->
         <div class="icon__wrapper">
           <div class="icon__overlay "></div>
           <svg class="icon__picture">
@@ -90,34 +97,27 @@ markup() {
 
         <!-- message -->
         <div class="message">
-          <h3 class="title-text">Success</h3>
-          <p class="body-text"> &nbsp;</p>
+          <h3 class="title-text">${this.options.type || "Success"}</h3>
+          <p class="body-text">${this.options.text || "&nbsp;"}</p>
         </div>
-        <!-- close btn -->
+        <!-- close button -->
         <div class="icon__wrapper">
           <svg class="icon__close">
             <use xlink:href="image/sprite.svg#icon-cancel-circle"></use>
           </svg>
         </div>
-      </div>
-    </div> `;
+      </div>`;
   }
-
 }
 
-
-
-function createContainer(position:string) {
+function createContainer(position: string) {
   const container = document.createElement("div");
   container.dataset.position = position;
   container.classList.add(`toast__container`);
   document.body.append(container);
-
   return container;
 }
 
-const toast = new Toast({ position: "top-right", text: "Hello world!",type:'info' });
+const toast = new Toast({ position: "top-right", text: "Hello world!", type: 'success', autoClose: true });
 
-// setTimeout(()=>{
-//   toast.remove()
-// },1000)
+
